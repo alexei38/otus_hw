e652a497c49a2bb5f89fd763187f46106733ea8e
Alexei Margasov <amargasov@naumen.ru>
2021-12-17 14:17:04 +0500

    LRU Cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,16 +13,68 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mutex    sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mutex.Lock()
+	old, ok := c.items[key]
+	c.mutex.Unlock()
+
+	if ok {
+		c.queue.Remove(old)
+	} else if c.queue.Len() >= c.capacity && c.queue.Len() > 0 {
+		removeItem := c.queue.Back()
+		removeCacheItem, ok := removeItem.Value.(*cacheItem)
+		if !ok {
+			panic("cache is corrupted")
+		}
+		c.mutex.Lock()
+		delete(c.items, removeCacheItem.key)
+		c.mutex.Unlock()
+		c.queue.Remove(removeItem)
+	}
+	cItem := newCacheItem(key, value)
+	item := c.queue.PushFront(cItem)
+	c.mutex.Lock()
+	c.items[key] = item
+	c.mutex.Unlock()
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mutex.Lock()
+	item, ok := c.items[key]
+	c.mutex.Unlock()
+	if ok {
+		c.queue.MoveToFront(item)
+		cItem, ok := item.Value.(*cacheItem)
+		if !ok {
+			panic("cache is corrupted")
+		}
+		return cItem.value, ok
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue = NewList()
+}
+
 type cacheItem struct {
 	key   Key
 	value interface{}
+}
+
+func newCacheItem(key Key, value interface{}) *cacheItem {
+	return &cacheItem{
+		key:   key,
+		value: value,
+	}
 }
 
 func NewCache(capacity int) Cache {
