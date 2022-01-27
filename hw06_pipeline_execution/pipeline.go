cada11a89534a18501c8f67bcdfcd186e83b2c5b
Alexei Margasov <amargasov@naumen.ru>
2022-01-28 04:33:23 +0500

    hw06 pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,44 @@ type (
 
 type Stage func(in In) (out Out)
 
+func sendToPipeline(done In, terminate Bi, in In, out Bi) {
+	for data := range in {
+		select {
+		case <-done:
+			close(out)
+			close(terminate)
+			return
+		case out <- data:
+		}
+	}
+	close(out)
+}
+
+func runStage(done In, in In, out Bi, stage Stage) {
+	for data := range stage(in) {
+		select {
+		case <-done:
+			close(out)
+			return
+		case out <- data:
+		}
+	}
+	close(out)
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	terminate := make(Bi)
+	toStage := make(Bi)
+	fromStage := make(Bi)
+
+	go sendToPipeline(done, terminate, in, toStage)
+	for i, stage := range stages {
+		stage := stage
+		if i > 0 {
+			toStage = fromStage
+			fromStage = make(Bi)
+		}
+		go runStage(terminate, toStage, fromStage, stage)
+	}
+	return fromStage
 }
