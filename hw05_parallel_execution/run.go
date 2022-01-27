36b5be5ca0d5adf782c343d810f8cdfcf42e2770
Alexei Margasov <amargasov@naumen.ru>
2022-01-27 23:49:27 +0500

    сделал небуферизированный канал, изменил проверку

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -23,32 +23,28 @@ type Task func() error
 func (t *TaskExecutor) runWorker(wg *sync.WaitGroup, taskCh <-chan Task) {
 	defer wg.Done()
 	for {
-		// если набралось достаточное количество ошибок - выходим из воркера
-		if atomic.LoadInt32(&t.errors) >= atomic.LoadInt32(&t.maxErrors) {
-			return
-		}
 		task, ok := <-taskCh
 		if !ok {
 			// выходим, если все обработали и канал закрыт
 			return
 		}
-		err := task()
-		if err != nil {
+		if err := task(); err != nil {
 			atomic.AddInt32(&t.errors, 1)
 		}
 	}
 }
 
 func (t *TaskExecutor) Run(tasks []Task) error {
-	// Буферизированный канал, чтобы не блокироваться на записи в канал
-	// если все воркеры закончат работу раньше времени из-за ошибок
-	taskCh := make(chan Task, len(tasks))
+	taskCh := make(chan Task)
 	wg := &sync.WaitGroup{}
 	wg.Add(t.workers)
 	for i := 0; i < t.workers; i++ {
 		go t.runWorker(wg, taskCh)
 	}
 	for _, task := range tasks {
+		if atomic.LoadInt32(&t.errors) >= t.maxErrors {
+			break
+		}
 		taskCh <- task
 	}
 	// Как записали все задачи в канал, закрываем канал и ждем, когда воркеры их обработают
