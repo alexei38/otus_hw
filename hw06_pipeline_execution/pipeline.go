ccbbfae5e30035da1568fbcc803ad9055ee0697d
Alexei Margasov <amargasov@naumen.ru>
2022-01-28 15:16:21 +0500

    fix после разбора дз

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,44 +8,32 @@ type (
 
 type Stage func(in In) (out Out)
 
-func sendToPipeline(done In, terminate Bi, in In, out Bi) {
-	for data := range in {
-		select {
-		case <-done:
-			close(out)
-			close(terminate)
-			return
-		case out <- data:
+func runStage(done In, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
 		}
-	}
-	close(out)
-}
-
-func runStage(done In, in In, out Bi, stage Stage) {
-	for data := range stage(in) {
-		select {
-		case <-done:
-			close(out)
-			return
-		case out <- data:
-		}
-	}
-	close(out)
+	}()
+	return out
 }
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	terminate := make(Bi)
-	toStage := make(Bi)
-	fromStage := make(Bi)
-
-	go sendToPipeline(done, terminate, in, toStage)
-	for i, stage := range stages {
-		stage := stage
-		if i > 0 {
-			toStage = fromStage
-			fromStage = make(Bi)
+	out := runStage(done, in)
+	for _, stage := range stages {
+		if stage == nil {
+			continue
 		}
-		go runStage(terminate, toStage, fromStage, stage)
+		out = stage(runStage(done, out))
 	}
-	return fromStage
+	return out
 }
